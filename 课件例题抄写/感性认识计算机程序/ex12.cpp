fdef47c8236513cc5a2698f06ff514d874932e7e
guaiu_glb <guaiu_glb@atguigu.com>
2019-10-19 11:54:15 +0800

    2019-10-19整合

---
+++ 课件例题抄写/感性认识计算机程序/ex12.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char a = '0';//用于存放用户输入的字母
+	cout<<"猜猜我是哪个字母，最多猜5次哦:"<<endl;
+	int i = 0;//用于记录猜过多少次了
+	for (i = 0; i < 5; i++)
+	{
+		cin>>a;
+		if(a == 'G')//如果猜中
+		{
+			cout<<"被你猜中了!"<<endl;
+			break;//终止循环
+		}
+		else  //如果没有猜中
+		cout<<"你猜错了!接着猜吧!"<<endl;
+	}
+	return 0;
+}
+
+
+
+//定义数组时数组不能为空，否则编译器报错
